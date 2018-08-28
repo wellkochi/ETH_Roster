015bd66dd4bafa4a74a8bf9c96b3b3707de08e0f
Gaoqi Zheng <zgqqi@126.com>
2018-08-28 16:19:05 -0700

    Update Roster.sol

--- contracts/Roster.sol
+++ contracts/Roster.sol
@@ -3,16 +3,18 @@ pragma solidity ^0.4.24;
 contract Roster{
 
     address owner;
-
+    
+    // setting up owner address to the contract caller
     constructor() public {
         owner = msg.sender;
     }
-
+    
    modifier onlyOwner {
        require(msg.sender == owner);
        _;
    }
-
+   
+   // instructor attribute
     struct Instructor {
         uint age;
         string fName;
@@ -20,14 +22,18 @@ contract Roster{
     }
 
     mapping (address => Instructor) instructors;
+    
+    // make an array of instructors
     address[] public instructorAccts;
-
+    
+    // event
     event instructorInfo(
        string fName,
        string lName,
        uint age
     );
-
+    
+    // set up a new instructor, as well as push it to the array
     function setInstructor(address _address, uint _age, string _fName, string _lName) onlyOwner public {
 
         instructors[_address].age = _age;
@@ -35,17 +41,22 @@ contract Roster{
         instructors[_address].lName = _lName;
 
         instructorAccts.push(_address) -1;
+        
+        // emit this instructor as an event
         emit instructorInfo(_fName, _lName, _age);
     }
-
+    
+    // get the array of instructors by address
     function getInstructors() view public returns(address[]) {
         return instructorAccts;
     }
-
+    
+    // get a specific instructor's age, firstName and lastName by taking instructor's address
     function getInstructor(address _address) view public returns (uint, string, string) {
         return (instructors[_address].age, instructors[_address].fName, instructors[_address].lName);
     }
-
+    
+    // return total number of instructors in the array
     function countInstructors() view public returns (uint) {
         return instructorAccts.length;
     }
